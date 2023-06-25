01ab330ae565a05c36f6ea1eda2648d3ac70b410
makaires77 <marcosaires1@gmail.com>
2023-06-24 21:17:05 -0300

    bateria05

--- teste_carga.ps1
+++ teste_carga.ps1
@@ -1,6 +1,6 @@
-$usersList = @(100, 200, 400)
+$usersList = @(50, 150, 250)
 $instancesList = @(1, 2, 3)
-$testDuration = "120s"
+$testDuration = "90s"
 $targetHost = "http://localhost:8080"
 
 # Mudar o diretório de trabalho atual para a pasta 'teste_carga'
@@ -11,7 +11,7 @@ foreach($u in $usersList) {
         Write-Host "Scaling wordpress to $i instances and testing with $u users"
         docker-compose down
         docker-compose up -d --scale wordpress=$i
-        Start-Sleep -s 30
+        Start-Sleep -s 15
 
         # Test each WordPress instance
         foreach($j in 1..$i) {
@@ -26,7 +26,7 @@ foreach($u in $usersList) {
         }
 
         Write-Host "Running locust for $testDuration with $u users"
-        locust -f locustfile.py --headless -u $u -r 10 --run-time $testDuration --host=$targetHost --csv=output_u_${u}_i_${i}
+        locust -f locustfile.py --headless -u $u -r 5 --run-time $testDuration --host=$targetHost --csv=output_u_${u}_i_${i}
         docker-compose down
     }
 }

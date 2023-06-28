08b2d950848fdfeca4b8ca8767bc1e50e1799958
makaires77 <marcosaires1@gmail.com>
2023-06-28 18:39:27 -0300

    reinicio

--- locustfile.py
+++ locustfile.py
@@ -4,13 +4,13 @@ class MyUser(HttpUser):
     wait_time = between(1, 3)
 
     @task(1)
-    def scenario_100_users(self):
-        self.client.get("/")
+    def scenario_post1(self):
+        self.client.get("http://localhost/?p=1")
 
     @task(2)
-    def scenario_200_users(self):
-        self.client.get("/")
+    def scenario_post2(self):
+        self.client.get("http://localhost/?p=8")
 
     @task(3)
-    def scenario_1000_users(self):
-        self.client.get("/")
+    def scenario_post3(self):
+        self.client.get("http://localhost/?p=11")

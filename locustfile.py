4bde9e8086e5b9abdf38bbea4961fa06106d57fc
makaires77 <marcosaires1@gmail.com>
2023-06-28 17:56:15 -0300

    ajuste reinicio

--- locustfile.py
+++ locustfile.py
@@ -1,25 +1,16 @@
-from locust import HttpUser, TaskSet, task, between
+from locust import HttpUser, task, between
 
-class UserBehavior(TaskSet):
-    host = "http://nginx"    # O endereço do servidor Nginx a ser testado
-    wait_time = between(5, 15)      # O tempo de espera entre as tarefas para cada usuário virtual é um valor aleatório entre 5 e 15 segundos
-    
-    @task
-    def index(self):
+class MyUser(HttpUser):
+    wait_time = between(1, 3)
+
+    @task(1)
+    def scenario_100_users(self):
         self.client.get("/")
 
-    @task
-    def blog_post_with_large_image(self):
-        self.client.get("/2023/06/17/post1/")
+    @task(2)
+    def scenario_200_users(self):
+        self.client.get("/")
 
-    @task
-    def blog_post_with_text(self):
-        self.client.get("/2023/06/15/post2/")
-
-    @task
-    def blog_post_with_small_image(self):
-        self.client.get("/2023/06/17/post3/")
-
-class WebsiteUser(HttpUser):
-    tasks = [UserBehavior]
-    wait_time = between(5, 15)
+    @task(3)
+    def scenario_1000_users(self):
+        self.client.get("/")

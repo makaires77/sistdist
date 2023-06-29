dad6760cfe731171ad27b2428c9991bcab74e0ea
makaires77 <marcosaires1@gmail.com>
2023-06-28 21:00:19 -0300

    atualizar para escalonar corretamente

--- teste_carga/teste_carga.ps1
+++ teste_carga/teste_carga.ps1
@@ -1,21 +1,22 @@
 $usersList     = @(100, 200, 300)
 $instancesList = @(1, 2, 3)
 $testDuration  = "90s"
-$targetHost    = "http://localhost:8080"
+$targetHost    = "http://localhost"
 
-# Mudar o diretório de trabalho atual para a pasta 'teste_carga'
-Set-Location -Path .\teste_carga
+# Mudar o diretório de trabalho atual para a pasta onde serão gerados dados de medição do teste
+Set-Location -Path C:\Users\marco\sistdist\teste_carga\bateria01
 
 foreach($u in $usersList) {
     foreach($i in $instancesList) {
         Write-Host "Scaling wordpress to $i instances and testing with $u users"
         docker-compose down
-        docker-compose up -d --scale wordpress=$i
-        Start-Sleep -s 15
+        docker-compose up -d
+        # docker-compose up -d --scale wordpress=$i
+        Start-Sleep -s 10
 
         # Test each WordPress instance
         foreach($j in 1..$i) {
-            $wp_url = "http://localhost:8080"
+            $wp_url = "http://localhost"
             try {
                 Invoke-WebRequest -Uri $wp_url -TimeoutSec 5 | Out-Null
                 Write-Host "Connectivity test passed for $wp_url"
